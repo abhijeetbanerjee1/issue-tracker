f8a2b8017b57950c014cc8cc427926b6aac79542
abhijeetbanerjee1 <abhijeet.banerjee883@gmail.com>
2024-05-07 19:46:35 -0400

    Populate Assignee component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+import { User } from "@prisma/client";
 import { Select, Text } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import { setLazyProp } from "next/dist/server/api-utils";
+import React, { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Text> Select an assignee...</Text>
@@ -11,7 +23,11 @@ const AssigneeSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Abhijeet Banerjee</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
       <Select.Separator />
